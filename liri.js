ecf7a731dc90dbdadd5f09dc017f9b8c2e82fa1a
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-09 21:43:47 -0400

    Commit

--- liri.js
+++ liri.js
@@ -6,6 +6,10 @@ if (commandName === 'concert-this') {
     concertThis();
 } else if (commandName === 'movie-this') {
     movieThis();
+} else if (commandName === "spotify-this-song") {
+    spotifyThis();
+} else if (commandName === "do-what-it-says") {
+    doWhatItSays();
 }
 
 function concertThis() {
@@ -15,3 +19,4 @@ function concertThis() {
 function movieThis() {
     console.log('do movie stuff');
 }
+
